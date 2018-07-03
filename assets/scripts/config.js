585915fd848b18115d4506c6f0f0717faf7d8ddd
samprimary <samprimary@gmail.com>
2018-07-03 14:04:22 -0400

    API URL now correctly points to production Heroku
    - https://desolate-wildwood-48917.herokuapp.com/

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://desolate-wildwood-48917.herokuapp.com/raiders',
   development: 'http://localhost:4741'
 }
 
