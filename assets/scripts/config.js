9d193465d4166eb9f27130840da41fcfb449e7c8
samprimary <samprimary@gmail.com>
2018-07-06 15:25:44 -0400

    Update production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  // production: 'https://desolate-wildwood-48917.herokuapp.com/raiders',
-  production: 'http://localhost:4741',
+  production: 'https://desolate-wildwood-48917.herokuapp.com/raiders',
+  // production: 'http://localhost:4741',
   development: 'http://localhost:4741'
 }
 
